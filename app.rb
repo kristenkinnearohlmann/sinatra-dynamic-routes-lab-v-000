e27b89b0f04805696c5168670c14eb470ed59491
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-19 01:25:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,11 @@ class App < Sinatra::Base
     @name = params[:name]
     "#{@name.reverse}"
   end
+
+  get 'square/:number' do
+    @num = params[:number].to_i
+    @square = @num * @num
+    "#{@square.to_s}"
+  end
+
 end
