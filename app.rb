0161da97e20ce283beaa66dd07b5ec58123b4997
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-19 02:41:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    @repeat_phrase = @num.times { @repeat_phrase += "#{@phrase} "}
+    @repeat_phrase = @num.times { @repeat_phrase = "#{@repeat_phrase} #{@phrase}"}
     "#{@repeat_phrase}"
   end
 
