9462e8618418ffe115e8396eb7fcde26725152ae
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-19 02:54:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,4 +31,23 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @operator = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    case @operator
+    when "add"
+      @result = @num1 + @num2
+    when "subtract"
+      @result = @num1 - @num2
+    when "multiply"
+      @result = @num1 * @num2
+    when "divide"
+      @result = @num1 / @num2
+    end
+
+    "#{@result}.to_s"
+  end
+
 end
